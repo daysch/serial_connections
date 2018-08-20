155d2ae8f6f80ad52855305b01ffa4e37443c61c
RS2D Scanner account <rs2d@jerryb.partners.org>
2018-08-20 19:29:00 -0400

    schedule via task scheduler

--- run_serials.py
+++ run_serials.py
@@ -1,42 +1,24 @@
 import easygui as e
 import traceback
-import webbrowser
-import os
+error_collection = ""
 
-from apscheduler.schedulers.blocking import BlockingScheduler
+# read equipment
+try:
+	import water_chiller
+except:
+	error_collection += "Failed to read water chiller status:\n\n"+traceback.format_exc()
 
-sched = BlockingScheduler()
+try:
+	import helium_compressor
+except:
+	error_collection += "Failed to read helium compressor status:\n\n"+traceback.format_exc()
 
-@sched.scheduled_job('interval', minutes=60)
-def timed_job():
+try:
+	import magnet_temp
+except:
+	error_collection += "Failed to read magnet temperature:\n\n"+traceback.format_exc()
 
-	error_collection = ""
-
-	# read equipment
-	try:
-		import water_chiller
-	except:
-		error_collection += "Failed to read water chiller status:\n\n"+traceback.format_exc()
-
-	try:
-		import helium_compressor
-	except:
-		error_collection += "Failed to read helium compressor status:\n\n"+traceback.format_exc()
-
-	try:
-		import magnet_temp
-	except:
-		error_collection += "Failed to read magnet temperature:\n\n"+traceback.format_exc()
-
-	# declare errors if appropriate
-	# if error_collection != "":
-	# 	print error_collection
-	# 	e.msgbox(error_collection)
-
-	path = os.path.dirname(os.path.abspath(__file__)) # relative directory path
-	errfile=open(path+"\\error.txt",'w')
-	errfile.write(error_collection)
-	errfile.close()
-	webbrowser.open(path+"\\error.txt")
-
-sched.start()
+# declare errors if appropriate
+if error_collection != "":
+	print error_collection
+	e.msgbox(error_collection)
